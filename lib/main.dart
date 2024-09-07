cb0aa7f1c1f0604fb6200ec402e565d68b67b846
Ameen <ameenvengara@gmail.com>
2024-09-07 15:58:45 +0530

    upated

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:desktop_drop/desktop_drop.dart';
 import 'package:exif/exif.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:image/image.dart' as img;
 import 'package:path/path.dart' as p;
 import 'package:permission_handler/permission_handler.dart';
@@ -41,7 +42,7 @@ class _WatermarkHomePageState extends State<WatermarkHomePage> {
   File? _landscapeWatermarkFile;
   String? _imageFolderPath;
   String? _outputFolderPath;
-  File? imageFile;
+  final bool _isProcessing = false;
 
   @override
   void initState() {
@@ -49,14 +50,12 @@ class _WatermarkHomePageState extends State<WatermarkHomePage> {
     super.initState();
   }
 
-  Future requestPermission() async {
+  Future<void> requestPermission() async {
     if (Platform.isAndroid || Platform.isIOS) {
-      if (await Permission.storage.request().isGranted) {
-        // Continue if granted
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text('Storage permission denied.'),
-        ));
+      if (!await Permission.storage.request().isGranted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Storage permission denied.')),
+        );
       }
     }
   }
@@ -67,131 +66,281 @@ class _WatermarkHomePageState extends State<WatermarkHomePage> {
       appBar: AppBar(
         title: const Text('Watermark App'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(
-              onPressed: _pickPortraitWatermarkFile,
-              child: const Text('Upload Portrait Watermark Image (PNG)'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _buildGrid(),
+                const SizedBox(height: 20),
+                if (!_isProcessing)
+                  ElevatedButton(
+                    onPressed: _areInputsSet() ? _applyWatermarkToImages : null,
+                    child: const Text('Apply Watermark'),
+                  )
+                else
+                  const Center(child: Text('Processing...')),
+                const SizedBox(
+                  height: 200,
+                )
+              ],
             ),
-            ElevatedButton(
-              onPressed: _pickLandscapeWatermarkFile,
-              child: const Text('Upload Landscape Watermark Image (PNG)'),
-            ),
-            ElevatedButton(
-              onPressed: _pickImageFolder,
-              child: const Text('Select Image Folder'),
-            ),
-            ElevatedButton(
-              onPressed: _pickOutputFolder,
-              child: const Text('Select Output Folder'),
-            ),
-            ElevatedButton(
-              onPressed: _applyWatermarkToImages,
-              child: const Text('Apply Watermark'),
-            ),
-            if (_portraitWatermarkFile != null || _landscapeWatermarkFile != null)
-              // Preview logic remains the same...
-              // Add any UI for previewing if needed
-              Container(),
-          ],
+          ),
         ),
       ),
     );
   }
 
-  Future<void> _pickPortraitWatermarkFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.image);
+  Widget _buildGrid() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      // crossAxisCount: 2,
+      // shrinkWrap: true,
+      // crossAxisSpacing: 16,
+      // mainAxisSpacing: 16,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        DropTarget(
+          onDragDone: (data) {
+            print(data);
 
-    if (result != null && result.files.single.extension == 'png') {
+            final file = data.files.first;
+            _pickPortraitWatermarkFile(File(file.path));
+          },
+          child: _buildGridItem(
+            icon: Icons.portrait,
+            title: 'Portrait Watermark',
+            file: _portraitWatermarkFile,
+            onTap: () {},
+          ),
+        ),
+        DropTarget(
+          onDragDone: (data) {
+            final file = data.files.first;
+            _pickLandscapeWatermarkFile(File(file.path));
+          },
+          child: _buildGridItem(
+            icon: Icons.landscape,
+            title: 'Landscape Watermark',
+            file: _landscapeWatermarkFile,
+            onTap: () {},
+          ),
+        ),
+        DropTarget(
+          onDragDone: (detail) {
+            _pickImageFolder(detail.files.first.path);
+          },
+          child: _buildGridItem(
+            icon: Icons.folder,
+            title: 'Image Folder',
+            filePath: _imageFolderPath,
+            onTap: () {},
+          ),
+        ),
+        DropTarget(
+          onDragDone: (details) {
+            _pickOutputFolder(details.files.first.path);
+          },
+          child: _buildGridItem(
+            icon: Icons.folder,
+            title: 'Output Folder',
+            filePath: _outputFolderPath,
+            onTap: () {
+              // _pickOutputFolder
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildGridItem({
+    required IconData icon,
+    required String title,
+    File? file,
+    String? filePath,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Text(title),
+          Container(
+            width: 200,
+            height: 200,
+            margin: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(color: Colors.grey),
+            ),
+            child: Stack(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              alignment: Alignment.center,
+              children: [
+                Icon(
+                  file != null || filePath != null ? Icons.check_circle : icon,
+                  size: 50,
+                  color: file != null || filePath != null ? Colors.green : Colors.grey,
+                ),
+                if (file != null)
+                  Image.file(
+                    file,
+                    fit: BoxFit.contain,
+                  ),
+                const SizedBox(height: 8),
+              ],
+            ),
+          ),
+          if (filePath != null) SizedBox(width: 200, child: Text(filePath)),
+          if (file != null) SizedBox(width: 200, child: Text(file.path)),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _pickPortraitWatermarkFile(File? result) async {
+    // FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.image);
+
+    if (result != null) {
+      print(result.path);
       setState(() {
-        _portraitWatermarkFile = File(result.files.single.path!);
+        _portraitWatermarkFile = result;
+        // _portraitWatermarkFile = File(result.files.single.path!);
       });
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text('Portrait watermark image selected successfully!'),
-      ));
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text('Please select a PNG file.'),
-      ));
     }
   }
 
-  Future<void> _pickLandscapeWatermarkFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.image);
-
-    if (result != null && result.files.single.extension == 'png') {
+  Future<void> _pickLandscapeWatermarkFile(File? result) async {
+    // FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.image);
+    if (result != null) {
       setState(() {
-        _landscapeWatermarkFile = File(result.files.single.path!);
+        _landscapeWatermarkFile = result;
       });
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text('Landscape watermark image selected successfully!'),
-      ));
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text('Please select a PNG file.'),
-      ));
     }
   }
 
-  Future<void> _pickImageFolder() async {
-    String? folderPath = await FilePicker.platform.getDirectoryPath();
-
+  Future<void> _pickImageFolder(String? folder) async {
     setState(() {
-      _imageFolderPath = folderPath;
+      _imageFolderPath = folder;
     });
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('Image folder selected successfully!'),
-    ));
-
-    await setWatermarkPreview();
+    // String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+    // if (selectedDirectory != null) {
+    //   setState(() {
+    //     _imageFolderPath = selectedDirectory;
+    //   });
+    // }
   }
 
-  Future<void> _pickOutputFolder() async {
-    String? folderPath = await FilePicker.platform.getDirectoryPath();
-
+  Future<void> _pickOutputFolder(String folder) async {
     setState(() {
-      _outputFolderPath = folderPath;
+      _outputFolderPath = folder;
     });
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('Output folder selected successfully!'),
-    ));
+    // String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+    // if (selectedDirectory != null) {
+    // setState(() {
+    //   _outputFolderPath = selectedDirectory;
+    // });
+    // }
   }
 
-  Future setWatermarkPreview() async {
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('Processing a preview...'),
-    ));
-
-    List<FileSystemEntity> images = Directory(_imageFolderPath!).listSync(recursive: true, followLinks: false);
-    List<String> validExtensions = ['jpg', 'jpeg', 'png'];
-
-    for (var file in images) {
-      String extension = p.extension(file.path).toLowerCase().replaceAll('.', '');
-
-      if (validExtensions.contains(extension)) {
-        try {
-          setState(() {
-            imageFile = File(file.path);
-          });
-
-          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            content: Text('Preview generated for the first valid image.'),
-          ));
-
-          break;
-        } catch (e, s) {
-          print("ERROR | $e");
-          print("TRACE | $s");
-          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            content: Text('Failed to generate preview.'),
-          ));
-        }
-      }
-    }
+  bool _areInputsSet() {
+    return _portraitWatermarkFile != null && _landscapeWatermarkFile != null && _imageFolderPath != null && _outputFolderPath != null;
   }
+
+  // Future<void> _pickPortraitWatermarkFile() async {
+  //   FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.image);
+
+  //   if (result != null && result.files.single.extension == 'png') {
+  //     setState(() {
+  //       _portraitWatermarkFile = File(result.files.single.path!);
+  //     });
+  //     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //       content: Text('Portrait watermark image selected successfully!'),
+  //     ));
+  //   } else {
+  //     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //       content: Text('Please select a PNG file.'),
+  //     ));
+  //   }
+  // }
+
+  // Future<void> _pickLandscapeWatermarkFile() async {
+  //   FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.image);
+
+  //   if (result != null && result.files.single.extension == 'png') {
+  //     setState(() {
+  //       _landscapeWatermarkFile = File(result.files.single.path!);
+  //     });
+  //     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //       content: Text('Landscape watermark image selected successfully!'),
+  //     ));
+  //   } else {
+  //     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //       content: Text('Please select a PNG file.'),
+  //     ));
+  //   }
+  // }
+
+  // Future<void> _pickImageFolder() async {
+  //   String? folderPath = await FilePicker.platform.getDirectoryPath();
+
+  //   setState(() {
+  //     _imageFolderPath = folderPath;
+  //   });
+  //   ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //     content: Text('Image folder selected successfully!'),
+  //   ));
+
+  //   await setWatermarkPreview();
+  // }
+
+  // Future<void> _pickOutputFolder() async {
+  //   String? folderPath = await FilePicker.platform.getDirectoryPath();
+
+  //   setState(() {
+  //     _outputFolderPath = folderPath;
+  //   });
+  //   ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //     content: Text('Output folder selected successfully!'),
+  //   ));
+  // }
+
+  // Future setWatermarkPreview() async {
+  //   ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //     content: Text('Processing a preview...'),
+  //   ));
+
+  //   List<FileSystemEntity> images = Directory(_imageFolderPath!).listSync(recursive: true, followLinks: false);
+  //   List<String> validExtensions = ['jpg', 'jpeg', 'png'];
+
+  //   for (var file in images) {
+  //     String extension = p.extension(file.path).toLowerCase().replaceAll('.', '');
+
+  //     if (validExtensions.contains(extension)) {
+  //       try {
+  //         setState(() {
+  //           imageFile = File(file.path);
+  //         });
+
+  //         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //           content: Text('Preview generated for the first valid image.'),
+  //         ));
+
+  //         break;
+  //       } catch (e, s) {
+  //         print("ERROR | $e");
+  //         print("TRACE | $s");
+  //         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //           content: Text('Failed to generate preview.'),
+  //         ));
+  //       }
+  //     }
+  //   }
+  // }
 
   Future<void> _applyWatermarkToImages() async {
     if (_portraitWatermarkFile == null || _landscapeWatermarkFile == null || _imageFolderPath == null || _outputFolderPath == null) {
@@ -252,26 +401,45 @@ class _WatermarkHomePageState extends State<WatermarkHomePage> {
     return finalImage;
   }
 
-  Future<Uint8List> _compressImageToTargetSize(img.Image image, int targetSizeInBytes) async {
-    int minQuality = 10;
-    int maxQuality = 100;
-    late Uint8List result;
-    int currentQuality = maxQuality;
+  Future<Uint8List> _compressImageToTargetSize(img.Image image) async {
+    try {
+      // Convert the img.Image object to Uint8List (JPEG format)
+      Uint8List imageBytes = img.encodeJpg(image, quality: 90) as Uint8List;
 
-    while (minQuality <= maxQuality) {
-      result = Uint8List.fromList(img.encodeJpg(image, quality: currentQuality));
-      int fileSize = result.lengthInBytes;
-
-      if (fileSize <= targetSizeInBytes) {
-        break;
-      } else {
-        maxQuality = currentQuality - 1;
-        currentQuality = (minQuality + maxQuality) ~/ 2;
-      }
+      // Optionally compress further with flutter_image_compress if needed
+      var result = await FlutterImageCompress.compressWithList(
+        imageBytes,
+        minWidth: 2300,
+        minHeight: 1500,
+        quality: 80,
+      );
+      return result;
+    } catch (e) {
+      print(e);
+      throw Exception('Failed to compress image');
     }
-
-    return result;
   }
+
+  // Future<Uint8List> _compressImageToTargetSize(img.Image image, int targetSizeInBytes) async {
+  //   int minQuality = 10;
+  //   int maxQuality = 100;
+  //   late Uint8List result;
+  //   int currentQuality = maxQuality;
+
+  //   while (minQuality <= maxQuality) {
+  //     result = Uint8List.fromList(img.encodeJpg(image, quality: currentQuality));
+  //     int fileSize = result.lengthInBytes;
+
+  //     if (fileSize <= targetSizeInBytes) {
+  //       break;
+  //     } else {
+  //       maxQuality = currentQuality - 1;
+  //       currentQuality = (minQuality + maxQuality) ~/ 2;
+  //     }
+  //   }
+
+  //   return result;
+  // }
 
   Future<void> saveCompressedImage(Uint8List imageBytes, String fileName) async {
     File outputFile = File(fileName);
@@ -283,7 +451,7 @@ class _WatermarkHomePageState extends State<WatermarkHomePage> {
     img.Image watermarkedImage = _applyWatermark(image, watermark);
 
     // Compress the image to a target size (e.g., 5MB)
-    Uint8List compressedImageBytes = await _compressImageToTargetSize(watermarkedImage, 5 * 1024 * 1024);
+    Uint8List compressedImageBytes = await _compressImageToTargetSize(watermarkedImage);
 
     // Save the processed image to the output folder with the same name as the original image
     String imageFileName = p.basename(imagePath);
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import desktop_drop
+import flutter_image_compress_macos
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
+  FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
